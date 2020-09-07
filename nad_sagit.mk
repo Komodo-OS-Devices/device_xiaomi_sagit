8a39277ed06cc1ae339952a0e80a57fcb0436da3
TRISF <trisfaizal@gmail.com>
2020-09-07 17:30:08 +0700

    fly nad

--- nad_sagit.mk
+++ nad_sagit.mk
@@ -21,20 +21,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sagit device
 $(call inherit-product, device/xiaomi/sagit/device.mk)
 
-# Inherit some common BlissRoms stuff.
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+# Inherit some common NAD stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
 # MiuiCamera
 $(call inherit-product-if-exists, vendor/apps/MiuiCamera/config.mk)
 
-PRODUCT_NAME := bliss_sagit
+PRODUCT_NAME := nad_sagit
 PRODUCT_DEVICE := sagit
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 6
 PRODUCT_MANUFACTURER := Xiaomi
-TARGET_BOOT_ANIMATION_RES := 1440
-BLISS_BUILDTYPE := OFFICIAL
-BLISS_DEVELOPER := kkce21
+
+TARGET_BOOT_ANIMATION_RES := 1080
+USE_PIXEL_CHARGING := true
+TARGET_FACE_UNLOCK := true
+
+NAD_BUILD_TYPE := OFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
